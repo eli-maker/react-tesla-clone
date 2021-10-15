486061ff07b0039dfe909a366bcb2575573fe44e
Eliana Moreno <eliana.morenoestrada@gmail.com>
2021-10-15 17:01:41 -0500

    tesla initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Header() {
+    return (
+        <Container>
+            <a>
+                
+                <img src="/images/logo.svg" alt=""/>
+
+
+
+            </a>
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+` 
